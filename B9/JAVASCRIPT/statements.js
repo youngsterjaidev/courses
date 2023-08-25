4f372485f5fdf7c2778a72133d3d56289c046534
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2023-08-25 11:01:31 +0530

    javascript

---
+++ B9/JAVASCRIPT/statements.js
@@ -0,0 +1,93 @@
+// CONDITIONAL STATEMET
+
+// if
+let Welcome = 5;
+let name = "rohit";
+
+// if
+// if (name === "jai") {
+//   console.log("Welcome");
+// } else {
+//   console.log("You are not jai");
+// }
+
+// elseif
+// if (name === "jai" || name === "udit" || name === "rohit") {
+//   console.log("Welcome team");
+// } else if (name === "foo") {
+//   console.log("Welcome foo");
+// } else {
+//   console.log("You are not allowed");
+// }
+
+// nested if statement
+// if (name === "jai") {
+//   console.log("Welcome Jai :)");
+// } else {
+//   if (name === "rohit") {
+//     console.log("Welcome Rohit :)");
+//   } else {
+//     if (name === "udit") {
+//       console.log("Welcome Udit :)");
+//     } else {
+//       console.log("You are not allowed !");
+//     }
+//   }
+// }
+
+// or
+
+// if (name === "jai") {
+//   console.log("Welcome Jai :)");
+// }
+
+// if (name === "rohit") {
+//   console.log("Welcome Rohit :)");
+// }
+
+// if (name === "udit") {
+//   console.log("Welcome Udit :)");
+// }
+
+// if (name !== "jai" && name !== "rohit" && name !== "udit") {
+//   console.log("You are not allowed !");
+// }
+
+// NOTE
+let num1 = 7;
+let num2 = 6;
+
+// if (num1 > num2) {
+//   console.log("The Num1 is greater than num2");
+// } else {
+//   if (num1 === num2) {
+//     console.log("Both Numbers are equal");
+//   } else {
+//     console.log("The Num2 is greater than num1");
+//   }
+// }
+
+// if (num1 > num2) {
+//   console.log("The Num1 is greater than num2");
+// } else if (num1 === num2) {
+//   console.log("Both Numbers are equal");
+// } else {
+//   console.log("The Num2 is greater than num1");
+// }
+
+// if (num1 > num2) {
+//   console.log("The Num1 is greater than num2");
+// }
+
+// if (num2 > num1) {
+//   console.log("The Num2 is greater than num1");
+// }
+
+// if (num2 === num1) {
+//   console.log("Both Numbers are equal");
+// }
+
+// {....}
+
+console.log("Welcome Team");
+console.log("Welcome Jai");
---
+++ B9/JAVASCRIPT/variables.js
@@ -0,0 +1,31 @@
+var data = "Some Changed Data";
+
+function Func() {
+  var newData = "Jam";
+  console.log(newData);
+}
+
+Func();
+
+console.log(data);
+
+// DATA TYPES
+
+// null
+var nullValue = null;
+
+// undedfined
+var und = undefined;
+
+// boolean
+var bool = true;
+var bool = false;
+
+// string
+var str = "data";
+
+// object
+var obj = { name: "jam" };
+
+// Array
+var arr = ["foo", "baar"];
