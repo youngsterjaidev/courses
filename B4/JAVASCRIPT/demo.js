870f1235402323d74fa6cb7b07b10286f354711d
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2023-09-11 22:36:09 +0530

    adding today work

---
+++ B4/JAVASCRIPT/demo.js
@@ -0,0 +1,32 @@
+// variables
+// let, var, const
+
+let name = "jai";
+
+const foo = "jam";
+
+console.log(name);
+
+function ClickMe() {
+  let lastName = "dev";
+
+  console.log(lastName);
+}
+
+ClickMe();
+
+// DATA Types
+let str = "string";
+let num = 5;
+let bool = true;
+let nullVal = null;
+let undefinedVal = undefined;
+let obj = { name: "Jai", phone: 3243432 };
+
+let animals = ["cat", "dog", "sparrow", 42342];
+
+// make an object with key like
+// name, address, phone, email, hobbies
+// address contain an another object that have
+// streetName and streetNo
+// hobbies will be arrary
