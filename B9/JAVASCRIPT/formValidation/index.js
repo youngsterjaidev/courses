3978edd8d7e70c1b1a1d1b983b6e85e0a2c70ab2
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2023-09-12 10:58:25 +0530

    adding the form validation

--- B9/JAVASCRIPT/formValidation/index.js
+++ B9/JAVASCRIPT/formValidation/index.js
@@ -3,13 +3,45 @@ let loginForm = document.querySelector("#loginForm");
 let loginEmail = document.querySelector("#loginEmail");
 let loginPassword = document.querySelector("#loginPassword");
 let loginInfo = document.querySelector("#loginInfo");
+let loginBtn = document.querySelector("#loginBtn");
 
 let signupForm = document.querySelector("#signupForm");
 let signupEmail = document.querySelector("#signupEmail");
 let signupPassword = document.querySelector("#signupPassword");
 let signupInfo = document.querySelector("#signupInfo");
+let signupBtn = document.querySelector("#signupBtn");
 
 let errors = [];
+
+// setting the intial styling
+signupForm.style.display = "none";
+signupBtn.style.backgroundColor = "#b2b2ff";
+
+// add event to the button
+// singup
+signupBtn.addEventListener("click", function () {
+  // make display block of the signup Form
+  signupForm.style.display = "block";
+
+  // make display none of the login form
+  loginForm.style.display = "none";
+
+  // make the signupBtn
+  signupBtn.style.backgroundColor = "#6161ff";
+  loginBtn.style.backgroundColor = "#b2b2ff";
+});
+
+loginBtn.addEventListener("click", function () {
+  // make display block of the signup Form
+  loginForm.style.display = "block";
+
+  // make display none of the login form
+  signupForm.style.display = "none";
+
+  // make the signupBtn
+  loginBtn.style.backgroundColor = "#6161ff";
+  signupBtn.style.backgroundColor = "#b2b2ff";
+});
 
 function updateInfo(msg, id) {
   console.log(msg);
@@ -37,14 +69,22 @@ loginForm.addEventListener("submit", (event) => {
     updateInfo("Form is submitted sucessfully !", "loginInfo");
 
     // check if the user is registered or not
-    if (
-      loginEmail.value === "test333@gmail.com" &&
-      loginPassword.value === "test000"
-    ) {
-      console.log("Welcome test user");
-    } else {
-      console.log("User is not registered !");
-    }
+    // if (
+    //   loginEmail.value === "test333@gmail.com" &&
+    //   loginPassword.value === "test000"
+    // ) {
+    //   console.log("Welcome test user");
+    // } else {
+    //   console.log("User is not registered !");
+    // }
+
+    // send the credential to the server for authentication
+
+    fetch("https://blackbaas.tech/users/", { method: "GET" })
+      .then((r) => r.json())
+      .then((c) => {
+        console.log(c);
+      });
   }
 
   console.log(errors);
